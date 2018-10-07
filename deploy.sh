5e4ed19cc6942893fe4ecb6a72d9770bec376629
MatkoUSKO <pialamar@gmail.com>
2018-10-07 03:02:11 +0100

    echo deployed files

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-to_deploy=($(ls -l | grep -E "^d" | cut -d' ' -f 9))
+to_deploy=($(ls -l | grep -E "^d" | cut -d' ' -f 10))
 
 for i in ${to_deploy[@]}; do
   if [ -f $i/deploy.sh ]; then
+    echo "deploying $i"
     $i/deploy.sh
   fi
 done
